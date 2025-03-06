647d515cdd613eeef2ebf13c90289dcc5c5bcc62
Nery Estudillo <Estudner000@warren.k12.12.in.us>
2025-03-06 13:34:05 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+numbers = [1, 2, 2, 4, 5, 6, 8, 10, 11, 14]
+
+numbers.remove(2)
+squared = [x * x for x in range(10)]
+
+squared = set(squared)
+squared = list(squared)
+
+squared.sort()
+
+print(squared)
